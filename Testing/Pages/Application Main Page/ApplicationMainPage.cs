2d0992abcfe7c19e20765e9c33392de583663423
joshman1019 <joshman1019@gmail.com>
2022-03-14 16:57:20 -0400

    Updates to testing application's MainPage to include an input field testing selection.

--- Testing/Pages/Application Main Page/ApplicationMainPage.cs
+++ Testing/Pages/Application Main Page/ApplicationMainPage.cs
@@ -1,4 +1,6 @@
 using ConsoleUIUtilities2; 
+using System.Windows; 
+using System; 
 
 namespace Testing
 {
@@ -44,17 +46,15 @@ namespace Testing
             });
             firstChoice.SetMenuTriggerKeys(new ConsoleKey[] { ConsoleKey.D1, ConsoleKey.NumPad1 });
 
-            secondChoice.SetMenuItemText("2) SECOND CHOICE"); 
-            secondChoice.SetMenuItemCommand(() =>
+            secondChoice.SetMenuItemText("2) SHOW INPUTS"); 
+            secondChoice.SetMenuItemCommand(async () =>
             {
-                Console.Clear();
-                Console.WriteLine("Second Choice Selected");
-                Console.WriteLine("Press ENTER to contimue");
-                Console.ReadLine();
+                ShowAndLoadInputs(0, 6, 15, ConsoleColor.Blue, ConsoleColor.Yellow, ConsoleColor.Yellow, ConsoleColor.Cyan, ConsoleColor.Yellow, ConsoleColor.Blue, ConsoleColor.White, true);
+                Console.ReadLine(); 
             });
             secondChoice.SetMenuTriggerKeys(new ConsoleKey[] { ConsoleKey.D2, ConsoleKey.NumPad2 });
 
-            thirdChoice.SetMenuItemText("3) THIRD CHOICE"); 
+            thirdChoice.SetMenuItemText("3) SHOW DIALOG"); 
             thirdChoice.SetMenuItemCommand(() =>
             {
                 Dialog dialog = new Dialog("Test Title", Lorem.LOREM_LONG_STRING);
@@ -66,20 +66,34 @@ namespace Testing
             }); 
             thirdChoice.SetMenuTriggerKeys(new ConsoleKey[] {ConsoleKey.D3, ConsoleKey.NumPad3 });
 
-            fourthChoice.SetMenuItemText("4) FOURTH CHOICE"); 
+            fourthChoice.SetMenuItemText("4) EXIT APPLICATION"); 
             fourthChoice.SetMenuItemCommand(() =>
             {
                 Close(() =>
                 {
-                    Console.Clear();
-                    Console.WriteLine("Application has terminated");
-                    applicationMenu.CallMenuBreakExternal();
+                    Environment.Exit(0); 
                 }); 
             });
             fourthChoice.SetMenuTriggerKeys(new ConsoleKey[] { ConsoleKey.D4, ConsoleKey.NumPad4 });
 
             // Fill Menu
             applicationMenu.AddMenuItemRange(new MenuItem[] {firstChoice, secondChoice, thirdChoice, fourthChoice});
+
+            // Input Items
+            InputFieldSet applicaitonInputs = new InputFieldSet();
+
+            // Create inputs
+            Input personName = new Input("personName");
+            Input personAddress = new Input("personAddress");
+            Input personCity = new Input("personCity");
+
+            // Setup Inputs
+            personName.SetInputLabel("NAME");
+            personAddress.SetInputLabel("ADDRESS");
+            personCity.SetInputLabel("CITY");
+
+            // Add inputs to page
+            SetInputFieldSet(applicaitonInputs); 
 
             // Show the page
             ShowAndLoadMenu(
