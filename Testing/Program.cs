38586a0f8c73a33e7716e3a55f020174c7391323
joshman1019 <joshman1019@gmail.com>
2022-04-29 17:05:34 -0400

    Updated testing application

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace Testing;
+﻿using ConsoleUIUtilities2; 
+
+namespace Testing;
 
 public static class Program
 {
     public static void Main()
     {
+        Console.SetWindowSize(120, 28); 
+        Console.SetBufferSize(120, 28);
         DisplayItemsTestPage page = new DisplayItemsTestPage(); 
         DisplayItemsContainer container = new DisplayItemsContainer(page);
         container.ShowPage();
